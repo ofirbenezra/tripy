df2877f7146447afbebe069d5203cd6fb7ed9386
Ofir Ben-Ezra <ofir.benezra@ruckuswireless.com>
2020-11-24 23:09:50 +0200

    Use ngrx/entity for fetching users

--- tripy-client/src/app/models/user.model.ts
+++ tripy-client/src/app/models/user.model.ts
@@ -1,7 +1,15 @@
-export interface User {
+export class User {
   id: string;
   firstName: string;
   lastName: string;
   phone?: string;
   email?: string;
+
+  constructor(id: string, firstName: string, lastName: string, phone: string, email?: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.email = email;
+  }
 }
